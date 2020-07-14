1ad505a90493ff0bb7c86ffcc1083f10db9b46cd
Hailey <ahn5737@gmail.com>
2020-07-14 00:01:12 -0700

    minor format changes in contact info

--- backtotop.js
+++ backtotop.js
@@ -1,4 +1,4 @@
-//1.Back to Top button
+//Back to Top button
 //Get the button:
 mybutton = document.getElementById("topbtn");
 
@@ -17,26 +17,4 @@ function scrollFunction() {
 function topFunction() {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-//2.Tool bar colour change with scroll
-
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-});
-
-//3.sticky navbar
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
 }
---
+++ navbar.js
@@ -0,0 +1,21 @@
+//Tool bar colour change with scroll
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+//sticky navbar
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
